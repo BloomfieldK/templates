f8485adeeb214f76ccd4009fbad55ac8f2181061
BloomfieldK <kevin.bloomfield9@outlook.fr>
2018-09-24 10:58:29 +0400

    Initial commit

---
+++ header.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	  
+	<title>Annuaire</title>
+
+	<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<h1>Annuaire des employés</h1>
+	 <body style="background-color: orange;">
